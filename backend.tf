9340385ce5001d6a4b8256dab593bda47421faeb
vinithmh <vinithmh2000@gmail.com>
2024-06-02 08:57:53 +0530

    backend -coomit

--- backend.tf
+++ backend.tf
@@ -8,17 +8,3 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "terraform_locks"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
